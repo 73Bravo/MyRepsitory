52182b352abf78b59fac9af02e71897388633b1f
73Bravo <wernerkruger@gmail.com>
2018-08-03 10:31:08 +0200

    bla bla

--- Werner/src/CopyFiles.java
+++ Werner/src/CopyFiles.java
@@ -12,31 +12,34 @@ public class CopyFiles {
 	static String driver;
 
 	public static void main(String[] args)  throws IOException {
-		driver  = "C:/Users/yj0007/Desktop/Benefit Statements/TRF/2016-10-11 to 2017-04-08/Driver.csv";
+		driver  = "C:/Users/yj0007/Desktop/Benefit Statements/TRF/2017-10-10 to 2018-04-09/Delivery_Projection_Statement_List.csv";
 		
 		Path driverDir = Paths.get(driver);
 		Path readFile = driverDir.resolve(driver);
 		BufferedReader br = Files.newBufferedReader(readFile,Charset.forName("UTF-8"));
 		String thisLine;
-	    String[] csv = null;
+		String fromFolder = "C:/Users/yj0007/Desktop/Benefit Statements/TRF/2017-10-10 to 2018-04-09/Projection_Statements_All/";
+		String toFolder = "C:/Users/yj0007/Desktop/Benefit Statements/TRF/2017-10-10 to 2018-04-09/Projection_Statements_Deliver/";
+//	    String[] csv = null;
 	    int b = 0,counter=0;
 	    while ((thisLine = br.readLine()) != null) { 
-	    	    csv = thisLine.split(",");
+/*	    	    csv = thisLine.split(",");
 	          	csv[0] = csv[0].replace(' ', '_');
 	          	csv[0] = csv[0].replace('\'', '_');
 	          	csv[0] = csv[0].replace('\\', '_');
 	          	csv[0] = csv[0].replace('/', '_');
-	          	csv[0] = csv[0].replace('?', '_');
+	          	csv[0] = csv[0].replace('?', '_');*/
 	           	
-	           	File fromFile = new File(csv[2] + csv[1] + "/" + csv[0]);
-	           	File toDir    = new File(csv[3] + csv[1]);
-	           	toDir.mkdir();
-	           	File toFile   = new File(csv[3] + csv[1] + "/" + csv[0]);
+//	           	File fromFile = new File(csv[2] + csv[1] + "/" + csv[0]);
+	    		File fromFile = new File(fromFolder + thisLine.trim());
+//	    		File toDir    = new File(csv[3] + csv[1]);
+//	           	toDir.mkdir();
+	           	File toFile   = new File(toFolder + thisLine.trim());
 	            CopyOption[] options = new CopyOption[]{
 	            	      StandardCopyOption.REPLACE_EXISTING,
 	            	      StandardCopyOption.COPY_ATTRIBUTES
 	            	    }; 
-	            System.out.println("File = " + csv[0]);
+	            System.out.println("File = " + thisLine);
 	           	Files.copy(fromFile.toPath(), toFile.toPath(), options);
             	if (b==1000) { 
             		System.out.println(" done: "+counter);
--- Werner/src/ListFiles.java
+++ Werner/src/ListFiles.java
@@ -20,11 +20,30 @@ public class ListFiles {
 //	                System.out.println( "Dir:" + f.getName() );
 	            }
 	            else {
-	                System.out.println( "File:" + f.getAbsolutePath() );
+//	                System.out.println( "File:" + f.getAbsolutePath() );
 	                String fname = f.getName();
 //	                listf.add(f.getAbsolutePath() + "," + fname.substring(fname.lastIndexOf("_") + 1,fname.lastIndexOf(".")) + ",");
-//	                listf.add("ren " + "C:\\Werner\\FormerMemberClaimForm20170227\\Images\\" + fname + " " + fname.substring(fname.indexOf("_PolNo_")+7, fname.length()));
-	                listf.add(f.getName());
+//	                listf.add("ren " + "C:\\Werner\\Tax_Cert\\2018\\TPF\\\"" + fname + "\" " + fname.substring(fname.indexOf("_")+1, fname.length()));
+//	                String toFileName = fname.substring(fname.indexOf("_")+1, fname.indexOf(".")) + "_" + fname.substring(0, fname.indexOf("_")) + ".PDF";
+//	                toFileName = toFileName.replace(' ', '_');
+//	                System.out.println(fname);
+/*	                long count = fname.chars().filter(ch -> ch =='.').count();
+	                if(count>1){
+	                	System.out.println(fname + "      " + count);
+	                }*/
+	                
+//	                if (fname.indexOf(",")>0){
+//	                	System.out.println(fname);
+//	                }
+//	                int qq = fname.lastIndexOf("PolicyEmpNo_");
+//	                String pers = fname.substring(fname.lastIndexOf("PersonId_") + 9,fname.lastIndexOf("PolicyEmpNo_"));
+//	                String pol = fname.substring(fname.lastIndexOf("PolicyEmpNo_") + 13,fname.lastIndexOf("pdf")-2);
+	                String empNo = fname.substring(fname.lastIndexOf("_")+1,fname.lastIndexOf("."));
+	                listf.add(empNo + "," + fname);	                
+//	                String xx = fname.substring(0, fname.indexOf("."));
+//	                System.out.println(xx + "," + fname);
+//	                listf.add(xx + "," + fname);
+//	                listf.add(fname);
 	            }
 	        }
 
@@ -34,12 +53,12 @@ public class ListFiles {
 			Collection<String> listf = new ArrayList<String>();
 			PrintWriter out = null;
 			try {
-				out = new PrintWriter("C:/Werner/CRF_FEI/201710List.txt");
+				out = new PrintWriter("C:/Werner/Tax_Cert/2018/TPF-Individual_Tax_certificates.txt");
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			}
 	    	ListFiles fw = new ListFiles();
-	        fw.walk("C:/Werner/CRF_FEI/201710",listf);
+	        fw.walk("C:/Werner/Tax_Cert/2018/TPF",listf);
 	        for (String line : listf) {
 	            out.println(line);
 	        }
